58465ed2a9df1c4d9f46df15eecb146abc5414d7
Kavitha0907 <168117493+Kavitha0907@users.noreply.github.com>
2024-05-10 15:45:12 +0530

    Create Power-of-pov.java

---
+++ Power-of-pov.java
@@ -0,0 +1,12 @@
+
+
+//User function Template for Java
+
+class Solution
+{
+    public long sum_of_square_evenNumbers(long n)
+    {
+        return ( 2*n*(n + 1)*(2*n + 1) ) / 3;
+        // Code here
+    }
+}
